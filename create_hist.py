2ba708284bdc116808e6557e5d2adeb866239d89
Kevin Feng <kjfeng@princeton.edu>
2019-11-02 18:12:36 -0400

    added histogram creation

--- create_hist.py
+++ create_hist.py
@@ -1,26 +1,30 @@
 # *****************************************************************************
-#  test.py
+#  creat_hist.py
 #  Author: Kevin Feng
 #  Description:
-#   playground
+#   outputs histogram for selected image
 # *****************************************************************************
 
 import cv2
 import math
 import numpy as np
 from sys import argv, stderr, exit
-import time
 from matplotlib import pyplot as plt
+
+def create_hist(img):
+    plt.hist(img.ravel(),256,[0,256])
+    plt.show()
 
 
 def main(argv):
     args = argv[1:]
+    if len(args) != 1:
+        print("Usage: python create_hist.py [img_path]", file=stderr)
     rawpath = r'{}'.format(args[0])
     img = cv2.imread(rawpath, 0)
 
     # find histogram using numpy.ravel()
-    plt.hist(img.ravel(),256,[0,256])
-    plt.show()
+    create_hist(img)
 
 
 
